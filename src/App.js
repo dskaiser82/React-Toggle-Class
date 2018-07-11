87b669dba2d08e59586a110fb3dc4b31290320d2
Dan Kaiser <dskaiser82@gmail.com>
2018-07-11 16:59:23 -0700

    have togglr

--- src/App.js
+++ src/App.js
@@ -6,12 +6,18 @@ class App extends Component {
   state = {
   isActive:true
   }
+
+  toggleClass = (e) => {
+    console.log("Woot")
+    this.setState({isActive: !this.state.isActive})
+  }
+
   render() {
     console.log(this.state)
     return (
       <div className="App">
         <div className="box teal lighten-2">
-          <a className="waves-effect waves-light btn">Click Me!</a>
+          <a onClick={this.toggleClass} className="waves-effect waves-light btn">Click Me!</a>
         </div>
       </div>
     );
