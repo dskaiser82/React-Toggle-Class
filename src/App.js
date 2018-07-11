f86f7453d4aedb23922998ddf4b83784e1f6a6e4
Dan Kaiser <dskaiser82@gmail.com>
2018-07-11 16:46:39 -0700

    added state

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+  isActive:true
+  }
   render() {
+    console.log(this.state)
     return (
       <div className="App">
         <div className="box teal lighten-2">
-          <a class="waves-effect waves-light btn">Click Me!</a>
+          <a className="waves-effect waves-light btn">Click Me!</a>
         </div>
       </div>
     );
